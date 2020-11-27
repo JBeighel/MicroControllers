5a23e6ed90d17f27c96e28daa94444987fc21bbf
JBeighel <vodobaas@gmail.com>
2020-11-27 16:05:01 -0500

    Strting I2C
    Creating the init and shutdown functions for the I2C bus.
    Need ioctl() to set slave address, then read()/write() to send data over the bus.
    I2C clock speed is set in /boot/config.txt with this line
    dtparam=i2c_arm=on,i2c_arm_baudrate=40000
    
    Don't think it can be altered at run time

--- Projects/RasPi-IO/I2C_RaspberryPi.c
+++ Projects/RasPi-IO/I2C_RaspberryPi.c
@@ -4,7 +4,7 @@
 */
 
 /*****	Includes	*****/
-
+	#include "I2C_RaspberryPi.h"
 
 /*****	Constants	*****/
 
@@ -19,7 +19,40 @@
 
 
 /*****	Prototypes 	*****/
-
+	eI2CReturns_t RasPiI2CShutdown(sI2CIface_t *pI2CIface);
 
 /*****	Functions	*****/
 
+eI2CReturns_t RasPiInitializeI2CBus(sI2CIface_t *pI2CIface, bool bActAsMaster, uint32_t nClockFreq, void *pHWInfo) {
+	sRasPiI2CHWInfo_t *pI2C = (sRasPiI2CHWInfo_t *)pHWInfo;
+	
+	//Setup the interface structure
+	I2CInterfaceInitialize(pI2CIface);
+	
+	pI2CIface->bMaster = bActAsMaster;
+	pI2CIface->nClockFreq = nClockFreq;
+	pI2CIface->pHWInfo = pHWInfo;
+	
+	//Set all the interface functions
+	pI2CIface->pfInitialize = &RasPiInitializeI2CBus;
+	pI2CIface->pfShutdown = &RasPiI2CShutdown;
+	
+	//Set up the hardware
+	pI2C->I2CFile = open(pI2C->pcFilePath, O_RDWR);
+	
+	if (pI2C->I2CFile < 0) {
+		return I2C_Fail_Unknown;
+	}
+	
+	return I2C_Success;
+}
+
+eI2CReturns_t RasPiI2CShutdown(sI2CIface_t *pI2CIface) {
+	sRasPiI2CHWInfo_t *pI2C = (sRasPiI2CHWInfo_t *)(pI2CIface->pHWInfo);
+	
+	if (pI2C->I2CFile >= 0) {
+		close(pI2C->I2CFile);
+	}
+	
+	return I2C_Success;
+}
--- Projects/RasPi-IO/I2C_RaspberryPi.h
+++ Projects/RasPi-IO/I2C_RaspberryPi.h
@@ -18,22 +18,32 @@
 	#include <sys/ioctl.h>
 	#include <linux/i2c-dev.h>
 	
+	#include "CommonUtils.h"
 	#include "I2CGeneralInterface.h"
 
 /*****	Constants	*****/
-
+	#define I2C_1_PORTINIT		RasPiInitializeI2CBus
+	
+	#define I2C_1_HWINFO		((void *)&(gI2CHWInfo[0]))
 
 /*****	Definitions	*****/
-
+	typedef struct sRasPiI2CHWInfo_t {
+		const char *pcFilePath;
+		int32_t I2CFile;
+	} sRasPiI2CHWInfo_t;
 
 /*****	Constants	*****/
+	const char cgI2C1File[] = "/dev/i2c-1";
 
+	sRasPiI2CHWInfo_t gI2CHWInfo [] = {
+		{ .pcFilePath = cgI2C1File, .I2CFile = 0, },
+	};
 
 /*****	Globals		*****/
-
+	int32_t gI2CFile;
 
 /*****	Prototypes 	*****/
-
+	eI2CReturns_t RasPiInitializeI2CBus(sI2CIface_t *pI2CIface, bool bActAsMaster, uint32_t nClockFreq, void *pHWInfo);
 
 /*****	Functions	*****/
 
