cc670bbe400784bdfe8f092af9c961ab86bc0b32
JBeighel <vodobaas@gmail.com>
2020-11-29 14:56:53 -0500

    Beginning GPIO expander driver
    Brought over all the definitions for the GPIO expander.  Starting on the control functions.

--- Projects/RasPi-IO/MCP23017Driver.c
+++ Projects/RasPi-IO/MCP23017Driver.c
@@ -7,7 +7,7 @@
 	#define __NAME_H
 
 /*****	Includes	*****/
-
+	#include "MCP23017Driver.h"
 
 /*****	Constants	*****/
 
@@ -25,7 +25,36 @@
 
 
 /*****	Functions	*****/
-
+eMCP23017Returns_t MCP23017Initialize(sMCP23017Info_t *pDev, eMCP23017Address_t eAddr, sI2CIface_t *pI2CIface) {
+	uint8_t nIOConVal, nRegVal, nCtr;
+	eI2CReturns_t nResult;
+	
+	//Setup the device object
+	pDev->nAddr = (uint8_t)eAddr;
+	pDev->pI2C = pI2CIface;
+	
+	//Set the initial configuration for the IO Expander.  Listing a bit sets it to 1, excluing it sets it to zero.
+	nIOConVal = MCP23017_BANK | MCP23017_SEQOP | MCP23017_HAEN;
+	pDev->pI2C->pfI2CWriteUint8Reg(pDev->pI2C, pDev->nAddr, MCP23017_B0_IOCON, nIOConVal); //Write to both possible IOCON addresses just to be sure
+	pDev->pI2C->pfI2CWriteUint8Reg(pDev->pI2C, pDev->nAddr, MCP23017_IOCON, nIOConVal);
+	
+	pDev->pI2C->pfI2CReadUint8Reg(pDev->pI2C, pDev->nAddr, MCP23017_IOCON, &nRegVal);
+	if (nRegVal != nIOConVal) { //Device did not store the values set
+		return MCP23017Fail_Unknown;
+	}
+	
+	//Set all pins to Inputs with pull ups
+	pDev->pI2C->pfI2CWriteUint8Reg(pDev->pI2C, pDev->nAddr, MCP23017_IODIRA, MCP23017_Pin_All);
+	pDev->pI2C->pfI2CWriteUint8Reg(pDev->pI2C, pDev->nAddr, MCP23017_IODIRB, MCP23017_Pin_All);
+	pDev->pI2C->pfI2CWriteUint8Reg(pDev->pI2C, pDev->nAddr, MCP23017_IPOLA, MCP23017_Pin_None);
+	pDev->pI2C->pfI2CWriteUint8Reg(pDev->pI2C, pDev->nAddr, MCP23017_IPOLA, MCP23017_Pin_None);
+	pDev->pI2C->pfI2CWriteUint8Reg(pDev->pI2C, pDev->nAddr, MCP23017_GPPUA, MCP23017_Pin_All);
+	pDev->pI2C->pfI2CWriteUint8Reg(pDev->pI2C, pDev->nAddr, MCP23017_GPPUB, MCP23017_Pin_All);
+	pDev->pI2C->pfI2CWriteUint8Reg(pDev->pI2C, pDev->nAddr, MCP23017_OLATA, MCP23017_Pin_None); //Not outputs by default, but have a predictable level when set
+	pDev->pI2C->pfI2CWriteUint8Reg(pDev->pI2C, pDev->nAddr, MCP23017_OLATA, MCP23017_Pin_None);
+	
+	return MCP23017_Success;
+}
 
 #endif
 
--- Projects/RasPi-IO/MCP23017Driver.h
+++ Projects/RasPi-IO/MCP23017Driver.h
@@ -13,13 +13,152 @@
 	#define __MCP23017DRIVER_H
 
 /*****	Includes	*****/
-
+	#include "I2CGeneralInterface.h"
 
 /*****	Constants	*****/
-
+	/**	@brief		Number of GPIO provided on the MCP23017 devices across all ports
+		@ingroup	mcp23017driver
+	*/
+	#define MCP23017_TOTALIOCNT		16
+	
+	/**	@brief		Number of GPIO provided on the MCP23017 devices on a single port
+		@ingroup	mcp23017driver
+	*/
+	#define MCP23017_PORTIOCNT		8
 
 /*****	Definitions	*****/
+	typedef enum eMCP23017Returns_t {
+		MCP23017Warn_Unknown	= -1,	/**< An unknown but recoverable error occurred */
+		MCP23017_Success		= 0,	/**< Operation completed successfully */
+		MCP23017Fail_Unknown	= 1,	/**< An unknown and unrecoverable occured */
+	} eMCP23017Returns_t;
 
+	/**	@brief		Values for the I2C address of the MCP23017
+		@details	The MCP23017 has a base address value that all of the chips use.  In addition there are 
+			3 pins that can add additional bits to that base address to allow up to 8 of these devices to be
+			connected to the same I2C bus.
+		@ingroup mcp23017driver
+	*/
+	typedef enum eMCP23017Address_t {
+		MCP23017_Base			= 0x20,	/**< Base address */
+		MCP23017_A0				= 0x01,	/**< Bit added by address pin 0 */
+		MCP23017_A1				= 0x02,	/**< Bit added by address pin 1 */
+		MCP23017_A2				= 0x04,	/**< Bit added by address pin 2 */
+	} eMCP23017Address_t;
+	
+	/**	@brief		Register addresses when IOCON register Bank (bit 7) is 1
+		@ingroup	mcp23017driver
+	*/
+	typedef enum eMCP23017RegsBank1_t {
+		MCP23017_IODIRA			= 0x00,	/**< Input Direction for GPIO 0-7: 1 input pin, 0 output pin */
+		MCP23017_IPOLA			= 0x01,	/**< Input Poliarity for GPIO 0-7: 1 register bit is opposite logic from input pin, 0 register bit is same logic as input pin */
+		MCP23017_GPINTENA		= 0x02,	/**< Interrupt on change for GPIO 0-7: 1 enables interupt A for changes on this pin, 0 disables interrupt A for changes on this pin */
+		MCP23017_DEFVALA		= 0x03,	/**< Compare register for interrupt for GPIO 0-7, will raise interrupt is the pin value does not match this value.  Only used based on INTCON register setting. */
+		MCP23017_INTCONA		= 0x04,	/**< Interrupt control for GPIO 0-7: 1 pin value compared to DEFVAL register, 0 pin value compared against previous value */
+		MCP23017_IOCON			= 0x05,	/**< Configuration register */
+		MCP23017_GPPUA			= 0x06,	/**< Pull up resistor for GPIO 0-7: 1 pull-up enabled, 0 pull-up disabled */
+		MCP23017_INTFA			= 0x07,	/**< Interrupt flag register for GPIO 0-7: 1 pin caused interrupt, 0 no interrupt pending */
+		MCP23017_INTCAPA		= 0x08,	/**< Interrupt captured value for GPIO 0-7: 1 pin was logic high, 0 pin was logic low */
+		MCP23017_GPIOA			= 0x09,	/**< Logic level on the pin for GPIO 0-7: 1 pin is logic high, 0 pin is logic low */
+		MCP23017_OLATA			= 0x0A,	/**< Output latch for GPIO 0-7: 1 set output to logic high, 0 set output to logic low */
+		MCP23017_IODIRB			= 0x10,	/**< Input Direction for GPIO 8-16: 1 input pin, 0 output pin */
+		MCP23017_IPOLB			= 0x11,	/**< Input Poliarity for GPIO 8-15: 1 register bit is opposite logic from input pin, 0 register bit is same logic as input pin */
+		MCP23017_GPINTENB		= 0x12,	/**< Interrupt on change for GPIO 8-15: 1 enables interupt B for changes on this pin, 0 disables interrupt B for changes on this pin */
+		MCP23017_DEFVALB		= 0x13,	/**< Compare register for interrupt for GPIO 8-15, will raise interrupt is the pin value does not match this value.  Only used based on INTCON register setting. */
+		MCP23017_INTCONB		= 0x14,	/**< Interrupt control for GPIO 8-15: 1 pin value compared to DEFVAL register, 0 pin value compared against previous value */
+		MCP23017_IOCONB			= 0x15,	/**< Dubplicate copy  of configuration register */
+		MCP23017_GPPUB			= 0x16,	/**< Pull up resistor for GPIO 8-15: 1 pull-up enabled, 0 pull-up disabled */
+		MCP23017_INTFB			= 0x17,	/**< Interrupt flag register for GPIO 8-15: 1 pin caused interrupt, 0 no interrupt pending */
+		MCP23017_INTCAPB		= 0x18,	/**< Interrupt captured value for GPIO 8-15: 1 pin was logic high, 0 pin was logic low */
+		MCP23017_GPIOB			= 0x19,	/**< Logic level on the pin for GPIO 8-15: 1 pin is logic high, 0 pin is logic low */
+		MCP23017_OLATB			= 0x1A,	/**< Output latch for GPIO 8-15: 1 set output to logic high, 0 set output to logic low */
+		
+		MCP23017_PortOffset		= 0x10, /**< Offset between port A and port B settings */
+	} eMCP23017RegsBank1_t;
+	
+	/**	@brief		Register addresses when IOCON register Bank (bit 7) is 0
+		@ingroup	mcp23017driver
+	*/
+	typedef enum eMCP23017RegsBank0_t {
+		MCP23017_B0_IODIRA		= 0x00,	/**< Input Direction for GPIO 0-7: 1 input pin, 0 output pin */
+		MCP23017_B0_IODIRB		= 0x01,	/**< Input Direction for GPIO 8-16: 1 input pin, 0 output pin */
+		MCP23017_B0_IPOLA		= 0x02,	/**< Input Poliarity for GPIO 0-7: 1 register bit is opposite logic from input pin, 0 register bit is same logic as input pin */
+		MCP23017_B0_IPOLB		= 0x03,	/**< Input Poliarity for GPIO 8-15: 1 register bit is opposite logic from input pin, 0 register bit is same logic as input pin */
+		MCP23017_B0_GPINTENA	= 0x04,	/**< Interrupt on change for GPIO 0-7: 1 enables interupt A for changes on this pin, 0 disables interrupt A for changes on this pin */
+		MCP23017_B0_GPINTENB	= 0x05,	/**< Interrupt on change for GPIO 8-15: 1 enables interupt B for changes on this pin, 0 disables interrupt B for changes on this pin */
+		MCP23017_B0_DEFVALA		= 0x06,	/**< Compare register for interrupt for GPIO 0-7, will raise interrupt is the pin value does not match this value.  Only used based on INTCON register setting. */
+		MCP23017_B0_DEFVALB		= 0x07,	/**< Compare register for interrupt for GPIO 8-15, will raise interrupt is the pin value does not match this value.  Only used based on INTCON register setting. */
+		MCP23017_B0_INTCONA		= 0x08,	/**< Interrupt control for GPIO 0-7: 1 pin value compared to DEFVAL register, 0 pin value compared against previous value */
+		MCP23017_B0_INTCONB		= 0x09,	/**< Interrupt control for GPIO 8-15: 1 pin value compared to DEFVAL register, 0 pin value compared against previous value */
+		MCP23017_B0_IOCON		= 0x0A,	/**< Configuration register */
+		MCP23017_B0_IOCONB		= 0x0B,	/**< Dubplicate copy  of configuration register */
+		MCP23017_B0_GPPUA		= 0x0C,	/**< Pull up resistor for GPIO 0-7: 1 pull-up enabled, 0 pull-up disabled */
+		MCP23017_B0_GPPUB		= 0x0D,	/**< Pull up resistor for GPIO 8-15: 1 pull-up enabled, 0 pull-up disabled */
+		MCP23017_B0_INTFA		= 0x0E,	/**< Interrupt flag register for GPIO 0-7: 1 pin caused interrupt, 0 no interrupt pending */
+		MCP23017_B0_INTFB		= 0x0F,	/**< Interrupt flag register for GPIO 8-15: 1 pin caused interrupt, 0 no interrupt pending */
+		MCP23017_B0_INTCAPA		= 0x10,	/**< Interrupt captured value for GPIO 0-7: 1 pin was logic high, 0 pin was logic low */
+		MCP23017_B0_INTCAPB		= 0x11,	/**< Interrupt captured value for GPIO 7-15: 1 pin was logic high, 0 pin was logic low */
+		MCP23017_B0_GPIOA		= 0x12,	/**< Logic level on the pin for GPIO 0-7: 1 pin is logic high, 0 pin is logic low */
+		MCP23017_B0_GPIOB		= 0x13,	/**< Logic level on the pin for GPIO 7-15: 1 pin is logic high, 0 pin is logic low */
+		MCP23017_B0_OLATA		= 0x14,	/**< Output latch for GPIO 0-7: 1 set output to logic high, 0 set output to logic low */
+		MCP23017_B0_OLATB		= 0x15,	/**< Output latch for GPIO 8-15: 1 set output to logic high, 0 set output to logic low */
+	} eMCP23017RegsBank0_t;
+	
+	/**	@brief		Masks for each pin in the MCP23017 registers
+		@details	All registers of the MCP23017 except IOCON have bits for each of the pins in an IO port.
+			These values are the individual bits for each IO, they can be used as a mask to read a specific 
+			setting or bit ORed together to set the values.
+		@ingroup	mcp23017driver
+	*/
+	typedef enum eMCP23017_Pin_t {
+		MCP23017_Pin_0			= 0x01,	/**< Pin 0 of the port (GPIO 0 or 8) */
+		MCP23017_Pin_1			= 0x02,	/**< Pin 1 of the port (GPIO 1 or 9) */
+		MCP23017_Pin_2			= 0x04,	/**< Pin 2 of the port (GPIO 2 or 10) */
+		MCP23017_Pin_3			= 0x08,	/**< Pin 3 of the port (GPIO 3 or 11) */
+		MCP23017_Pin_4			= 0x10,	/**< Pin 4 of the port (GPIO 4 or 12) */
+		MCP23017_Pin_5			= 0x20,	/**< Pin 5 of the port (GPIO 5 or 13) */
+		MCP23017_Pin_6			= 0x40,	/**< Pin 6 of the port (GPIO 6 or 14) */
+		MCP23017_Pin_7			= 0x80,	/**< Pin 7 of the port (GPIO 7 or 15) */
+		
+		MCP23017_Pin_None		= 0x00,
+		MCP23017_Pin_All		= 0xFF,	/**< All 8 pins of the port */
+	} eMCP23017_Pin_t;
+	
+	/**	@brief		Values to specify the two ports of GPIO available on the MCP23017
+		@ingroup	mcp23017driver
+	*/
+	typedef enum eMCP23017_Port_t {
+		MCP23017_PortA			= 0x00,
+		MCP23017_PortB			= 0x08,
+	} eMCP23017_Port_t;
+	
+	/**	@brief		Flags to specify the modes that a GPIO should use
+		@ingroup	mcp23017driver
+	*/
+	typedef enum eMCP23017_PinMode_t {
+		MCP23017_Input			= 0x01,
+		MCP23017_Output			= 0x02,
+		MCP23017_PullUp			= 0x04,
+		MCP23017_ReverseLogic	= 0x08,
+		MCP23017_IntOnPinChange	= 0x10,
+		MCP23017_IntOnCompare	= 0x20,
+		MCP23017_IntCompareVal	= 0x40,
+	} eMCP23017_PinMode_t;
+	
+	enum eMCP23017_RegIOCON_t {
+		MCP23017_BANK			= 0x80,	/**< Controls register addressing: 1 pins separated into banks, 0 registers are sequential */
+		MCP23017_MIRROR			= 0x40,	/**< Controls interrupt pin connection: 1 pins are internally connected, 0 INTA is associated with Port A and INTB is associated with Port B */
+		MCP23017_SEQOP			= 0x20,	/**< Sequential operations mode: 1 disabled so address pointer does not increment, 0 enabled so address pointer increments */
+		MCP23017_DISSLW			= 0x10,	/**< Slew Rate control: 1 slew rate disabled, 0 slew rate enabled */
+		MCP23017_HAEN			= 0x08,	/**< Hardware address enable: 1 enables address pins, 0 disables address pins */
+		MCP23017_ODR			= 0x04,	/**< Control interrupt outputs: 1 open-drain output, 0 active driver output */
+		MCP23017_INTPOL			= 0x02,	/**< Polarity of interrupt pins for active driver ouput: 1 Active high, 0 Active low */
+	};
+
+	typedef struct sMCP23017Info_t {
+		uint8_t nAddr;
+		sI2CIface_t *pI2C;
+	} sMCP23017Info_t;
 
 /*****	Constants	*****/
 
@@ -28,7 +167,24 @@
 
 
 /*****	Prototypes 	*****/
-
+	eMCP23017Returns_t MCP23017Initialize(sMCP23017Info_t *pDev, eMCP23017Address_t eAddr, sI2CIface_t *pI2CIface);
+	
+	/**	@brief		Set the mode of operation for one of the GPIO pins
+		@param		pDev		The peripheral device object to use
+		@param		nPinNum		Number of the pin to read; 0-7 for port A, 8-15 for port B
+		@param		eMode		Bitwise OR of all mode flahs to set for this pin
+		@return		True if the flags are valid and were correctly applied, false on any error
+	*/
+	eMCP23017Returns_t MCP23017PinModeByPin(sMCP23017Info_t *pDev, uint8_t nPinNum, eMCP23017_PinMode_t eMode);
+	
+	/**	@brief		Set the mode of operation for one of the GPIO pins
+		@param		pDev		The peripheral device object to use
+		@param		ePort		Which port the pin is on, Port A is 0-7 and Port B is 8-15
+		@param		ePin		Which pin in the port is to be set
+		@param		eMode		Bitwise OR of all mode flahs to set for this pin
+		@return		True if the flags are valid and were correctly applied, false on any error
+	*/
+	eMCP23017Returns_t MCP23017PinModeByPort(sMCP23017Info_t *pDev, eMCP23017_Port_t ePort, eMCP23017_Pin_t ePin, eMCP23017_PinMode_t eMode);
 
 /*****	Functions	*****/
 
