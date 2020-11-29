ea8ab8b0932e77d2394421e2cae1ad9d18e6dd7b
JBeighel <vodobaas@gmail.com>
2020-11-28 20:19:13 -0500

    Adding GPIO driver
    Need an I2C device driver to test the I2C implementation with.  Since the MCP 23017 GPIO expander needs ported to the general interface setup going to use that.

---
+++ Projects/RasPi-IO/MCP23017Driver.c
@@ -0,0 +1,31 @@
+/**	File:	MCP23017Driver.c
+	Author:	J. Beighel
+	Date:	11-28-2020
+*/
+
+#ifndef __NAME_H
+	#define __NAME_H
+
+/*****	Includes	*****/
+
+
+/*****	Constants	*****/
+
+
+/*****	Definitions	*****/
+
+
+/*****	Constants	*****/
+
+
+/*****	Globals		*****/
+
+
+/*****	Prototypes 	*****/
+
+
+/*****	Functions	*****/
+
+
+#endif
+
---
+++ Projects/RasPi-IO/MCP23017Driver.h
@@ -0,0 +1,37 @@
+/**	@defgroup	mcp23017driver
+	@brief		
+	@details	v0.1
+	#Description
+	
+	#File Information
+		File:	MCP23017Driver.h
+		Author:	J. Beighel
+		Date:	11-28-2020
+*/
+
+#ifndef __MCP23017DRIVER_H
+	#define __MCP23017DRIVER_H
+
+/*****	Includes	*****/
+
+
+/*****	Constants	*****/
+
+
+/*****	Definitions	*****/
+
+
+/*****	Constants	*****/
+
+
+/*****	Globals		*****/
+
+
+/*****	Prototypes 	*****/
+
+
+/*****	Functions	*****/
+
+
+#endif
+
