6dcabe41542e8885ea8785ccb16dec370c5e3262
JBeighel <vodobaas@gmail.com>
2021-01-26 18:46:42 -0500

    New branch for 7seg LED driver
    Creating a branch for working on a 8 by 7 segment display driver.
    Created the project and some initial functions to begin working from.

---
+++ Projects/MAX7219/MAX7219Driver.c
@@ -0,0 +1,78 @@
+/**	File:	MAX7219Driver.c
+	Author:	J. Beighel
+	Date:	2021-01-26
+*/
+
+/*****	Includes	*****/
+
+
+/*****	Defines		*****/
+	
+
+/*****	Definitions	*****/
+
+
+/*****	Constants	*****/
+
+
+/*****	Globals		*****/
+
+
+/*****	Prototypes 	*****/
+eMAX7219Return_t MAX7219WriteValue(sMAX7219Info_t *pDev, eMAX7219Reg_t nReg, uint8_t nValue);
+
+/*****	Functions	*****/
+
+eMAX7219Return_t MAX7219Initialize(sMAX7219Info_t *pDev, uint8_t nNumDigits, GPIOID_t nChipSel, sGPIOIface_t *pGpioDev, sSPIIface_t *pSpiDev) {
+	//Setup device object
+	pDev->pGpio = pGpio;
+	pDev->pSpi = pSpi;
+	pDev->nChipSelect = nChipSel;
+	
+	//Sanitize this input
+	nNumDigits &= MAX7219SCAN_Mask;
+	
+	//Setup the hardware
+	pDev->pGPio->pfSetModeByPin(pDev->pGpio, pDev->nChipSelect, GPIO_DigitalOutput);
+	pDev->pGPio->pfDigitalWriteByPin(pDev->pGpio, pDev->nChipSelect, true);
+	
+	//Assume the device is just powering up, and set all registers
+	MAX7219WriteValue(pDev, MAX7219_SCANLIMIT, nNumDigits);
+	MAX7219WriteValue(pDev, MAX7219_DECODEMODE, MAX7219_DecodeNone);
+	MAX7219WriteValue(pDev, MAX7219_SHUTDOWN, MAX7219SHUTDWN_Norm);
+	MAX7219WriteValue(pDev, MAX7219_INTENSITY, MAX7219INTEN_Min);
+	MAX7219WriteValue(pDev, MAX7219_DISPLAYTEST, MAX7219DISPTST_Off);
+	
+	return Success;
+}
+
+eMAX7219Return_t MAX7219Initialize(sMAX7219Info_t *pDev, uint8_t nDigit, eMAX7219Led_t eLights) {
+	if (nDigit >= MAX7219_MAXDIGITS) {
+		return Fail_Invalid;
+	} else {
+		nDigit += 1; //Switch form zero based to one based indexing
+	}
+	
+	MAX7219WriteValue(pDev, nDigit, eLights);
+	
+	return Success;
+}
+
+
+eMAX7219Return_t MAX7219WriteValue(sMAX7219Info_t *pDev, eMAX7219Reg_t nReg, uint8_t nValue) {
+	uint8_t nByte;
+	
+	//Begin the transfer
+	pDev->pGPio->pfDigitalWriteByPin(pDev->pGpio, pDev->nChipSelect, false);
+	pDev->pSpi->pfBeginTransfer(pDev->pSpi);
+	
+	//Send register address, then value
+	pDev->pSpi->pfTransferByte(pDev->pSpi, nReg, &nByte);
+	pDev->pSpi->pfTransferByte(pDev->pSpi, nValue, &nByte);
+	
+	//End the transfer
+	pDev->pSpi->pfEndTransfer(pDev->pSpi);
+	pDev->pGPio->pfDigitalWriteByPin(pDev->pGpio, pDev->nChipSelect, true);
+	
+	return Success;
+}
---
+++ Projects/MAX7219/SPI_RaspberryPi.h
@@ -0,0 +1,72 @@
+/**	@defgroup	spiraspberrypi
+	@brief		SPI General Interface implementation for Raspberry Pi
+	@details	v0.2
+	#Description
+	
+	#File Information
+		File:	SPI_RaspberryPi.h
+		Author:	J. Beighel
+		Date:	2021-01-22
+*/
+
+#ifndef __SPIRASPBERRYPI_H
+	#define __SPIRASPBERRYPI_H
+
+/*****	Includes	*****/
+	#include <stdio.h>
+	#include <string.h>
+
+	#include <unistd.h>
+	#include <fcntl.h>
+	#include <errno.h>
+	#include <sys/ioctl.h>
+	#include <linux/spi/spidev.h>
+	
+	#include "CommonUtils.h"
+	#include "SPIGeneralInterface.h"
+
+/*****	Defines		*****/
+	/**	@brief		Function to call to initialize the first SPI bus
+		@ingroup	spiraspberrypi
+	*/
+	#define SPI_INIT			RasPiSPIPortInitialize
+	
+	/**	@brief		Hardware information for the first SPI bus
+		@ingroup	spiraspberrypi
+	*/
+	#define SPI_1_HWINFO		((void *)&(gSPIHWInfo[0]))
+	
+	/**	@brief		SPI 1 hardware object
+		@ingroup	spiraspberrypi
+	*/
+	#define SPI_1_CAPS			(SPI_Configure | SPI_BeginTransfer | SPI_EndTransfer | SPI_BiDir1Byte)
+	
+	/**	@brief		Number of bits included in each word of the SPI transfer
+		@ingroup	spiraspberrypi
+	*/
+	#define RASPISPI_BITSPERWORD	8
+
+/*****	Definitions	*****/
+	/**	@brief		Structure holding information on the SPI Hardware
+		@ingroup	i2craspberrypi
+	*/
+	typedef struct sRasPiSPIHWInfo_t {
+		const char *pcFilePath;	/**< Path to the filesystem object for this bus */
+		int32_t SPIFile;		/**< File handle to use when interacting with this bus */
+		int32_t nLastErr;		/**< Last error reported by the OS SPI function calls */
+	} sRasPiSPIHWInfo_t;
+
+/*****	Constants	*****/
+	extern sRasPiSPIHWInfo_t gSPIHWInfo[];
+
+/*****	Globals		*****/
+
+
+/*****	Prototypes 	*****/
+	eSPIReturn_t RasPiSPIPortInitialize(sSPIIface_t *pIface, void *pHWInfo, uint32_t nBusClockFreq, eSPIDataOrder_t eDataOrder, eSPIMode_t eMode);
+
+/*****	Functions	*****/
+
+
+#endif
+
