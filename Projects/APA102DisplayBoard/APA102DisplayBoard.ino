f5fda1b8b42007c2c47fa5c94d080e1de8640d2f
JBeighel <vodobaas@gmail.com>
2021-08-03 07:47:32 -0400

    More patterns
    Added a bunch of new color patterns and orders.

--- Projects/APA102DisplayBoard/APA102DisplayBoard.ino
+++ Projects/APA102DisplayBoard/APA102DisplayBoard.ino
@@ -25,9 +25,11 @@
 
   #define TICKS_LEDCHANGE 200
 
+  #define TIME_DEBOUNCE   50
+
   #define NUM_LEDS        28
 
-  #define PATTERN_NUM     5
+  #define PATTERN_NUM     9
   #define PATTERN_MAXLEN  64
   #define PATTERN_ORDHOLD 28
 
@@ -103,47 +105,9 @@
       (eAPA102Color_t)PATTERN_END
     },
     {
-      APA102_DimRed,
-      APA102_DimRed,
-      APA102_DimRed,
-      APA102_DimRed,
-      APA102_DimRed,
-      APA102_DimRed,
-      APA102_DimRed,
-      APA102_DimOrange,
-      APA102_DimOrange,
-      APA102_DimOrange,
-      APA102_DimOrange,
-      APA102_DimOrange,
-      APA102_DimOrange,
-      APA102_DimOrange,
-      APA102_DimYellow,
-      APA102_DimYellow,
-      APA102_DimYellow,
-      APA102_DimYellow,
-      APA102_DimYellow,
-      APA102_DimYellow,
-      APA102_DimYellow,
-      APA102_DimGreen,
-      APA102_DimGreen,
-      APA102_DimGreen,
-      APA102_DimGreen,
-      APA102_DimGreen,
-      APA102_DimGreen,
-      APA102_DimGreen,
+      APA102_DimPink,
+      APA102_DimCyan,
       APA102_DimBlue,
-      APA102_DimBlue,
-      APA102_DimBlue,
-      APA102_DimBlue,
-      APA102_DimBlue,
-      APA102_DimBlue,
-      APA102_DimBlue,
-      APA102_DimPurple,
-      APA102_DimPurple,
-      APA102_DimPurple,
-      APA102_DimPurple,
-      APA102_DimPurple,
-      APA102_DimPurple,
       APA102_DimPurple,
       
       (eAPA102Color_t)PATTERN_END
@@ -153,7 +117,6 @@
       APA102_DimOrange,
       APA102_DimYellow,
       APA102_DimGreen,
-      APA102_DimBlue,
       APA102_DimBlue,
       APA102_DimPurple,
       APA102_DimPink,
@@ -166,8 +129,72 @@
       APA102_DimYellow,
       APA102_DimGreen,
       APA102_DimBlue,
+      APA102_DimPurple,
+      APA102_DimPink,
+      
+      (eAPA102Color_t)PATTERN_END
+    },
+    {
+      APA102_DimRed,
+      APA102_DimOrange,
+      APA102_DimYellow,
+      APA102_DimGreen,
       APA102_DimBlue,
       APA102_DimPurple,
+      APA102_DimPink,
+      
+      (eAPA102Color_t)PATTERN_END
+    },
+    {
+      APA102_DimRed,
+      APA102_DimOrange,
+      APA102_DimYellow,
+      APA102_DimGreen,
+      APA102_DimBlue,
+      APA102_DimPurple,
+      APA102_DimPink,
+      
+      (eAPA102Color_t)PATTERN_END
+    },
+    {
+      APA102_DimRed,
+      APA102_DimOrange,
+      APA102_DimYellow,
+      APA102_DimGreen,
+      APA102_DimBlue,
+      APA102_DimPurple,
+      APA102_DimPink,
+      
+      (eAPA102Color_t)PATTERN_END
+    },
+    {
+      APA102_DimCyan,
+      APA102_DimCyan,
+      APA102_DimCyan,
+      APA102_DimCyan,
+      APA102_DimCyan,
+      APA102_DimCyan,
+      APA102_DimCyan,
+      APA102_DimBlue,
+      APA102_DimBlue,
+      APA102_DimBlue,
+      APA102_DimBlue,
+      APA102_DimBlue,
+      APA102_DimBlue,
+      APA102_DimBlue,
+      APA102_DimPurple,
+      APA102_DimPurple,
+      APA102_DimPurple,
+      APA102_DimPurple,
+      APA102_DimPurple,
+      APA102_DimPurple,
+      APA102_DimPurple,
+      APA102_DimPink,
+      APA102_DimPink,
+      APA102_DimPink,
+      APA102_DimPink,
+      APA102_DimPink,
+      APA102_DimPink,
       APA102_DimPink,
       
       (eAPA102Color_t)PATTERN_END
@@ -190,14 +217,11 @@
       1,
     },
     {
-      0,  7, 14, 21,
-      1,  8, 15, 22,
-      2,  9, 16, 23,
-      3, 10, 17, 24,
-      4, 11, 18, 25,
-      5, 12, 19, 26,
-      6, 13, 20, 27,
-      1,
+      0,  6,  10,    16, 18, 22, 26, //This order does chevrons
+      1,  5,  7, 13, 17,     23, 25,
+      2,  4,  8, 12, 14, 20, 24,
+      3,      9, 11, 15, 19, 21, 27, 
+      7,
     },
     {
       0,  1,  2,  3,  4,  5,  6, 
@@ -215,6 +239,47 @@
       5, 12, 19, 26,
       6, 13, 20, 27,
       4, //Hold of 4 means every column gets a color (with this order)
+    },
+    {
+      3, 10, 17, 24,
+      4, 11, 18, 25,
+      2,  9, 16, 23,
+      5, 12, 19, 26,
+      1,  8, 15, 22,
+      6, 13, 20, 27,
+      0,  7, 14, 21,
+      8, //Hold of 8 means every two columns gets a color
+    },
+    {
+      0,  7, 14, 21,
+      6, 13, 20, 27,
+      
+      1,  8, 15, 22,
+      5, 12, 19, 26,
+      
+      4, 11, 18, 25,
+      2,  9, 16, 23,
+      
+      3, 10, 17, 24,
+      8, //Hold of 8 means every two columns gets a color
+    },
+    {
+      0,  7, 20, 27, //This order pairs the rows
+      1,  8, 19, 26, //But moves the paris in opposite directions
+      2,  9, 18, 25,
+      3, 10, 17, 24,
+      4, 11, 16, 23,
+      5, 12, 15, 22,
+      6, 13, 14, 21,
+      4, 
+    },
+    {
+      0,  1,  2,  3,  4,  5,  6,
+      13, 20, 
+      27, 26, 25, 24, 23, 22, 21,
+      14, 15, 16, 17, 18, 19,
+      12, 11, 10,  9,  8,  7,
+      1,
     },
   };
   
@@ -242,17 +307,35 @@ void setup() {
 #define GetBlue(nColor)   ((nColor & APA102_BlueMask) >> APA102_BlueShift)
 
 void loop() {
-  static bool bLastBtn = false;
+  static bool bLastBtn = false, bBtnPressed = false;
   static uint32_t tLastChange = 0;
+  static uint32_t tDebounce = 0;
   static uint32_t nPatternStartIdx = 0;
   static uint32_t nPatternIdx = 0;
   static uint32_t nPatternLen = 0;
   
   bool bBtnDown;
   uint32_t tCurrTime, nLedCtr, nPatternCtr;
+
+  tCurrTime = gTime.pfGetTicks();
   
   gGPIO.pfDigitalReadByPin(&gGPIO, PIN_BUTTON, &bBtnDown);
+  if (bBtnDown == false) {
+    bLastBtn = false;
+    tDebounce = 0;
+  }
+  
   if ((bBtnDown == true) && (bBtnDown != bLastBtn)) {
+    tDebounce = tCurrTime;
+    bLastBtn = bBtnDown;
+  }
+
+  if ((tDebounce - tCurrTime >= TIME_DEBOUNCE) && (tDebounce != 0) && (bBtnDown == true) && (bLastBtn == true)) {
+    bBtnPressed = true; // debounce time is up, send press event
+    tDebounce = 0; //No debounce time means we're not debouncing
+  }
+
+  if (bBtnPressed == true) {
     Serial.println("Button pressed!");
     nPatternIdx += 1;
     nPatternStartIdx = 0;
@@ -262,8 +345,9 @@ void loop() {
     }
 
     nPatternLen = 0; //mark the length unknown
+    
+    bBtnPressed = false; //Button handled, clear the event
   }
-  bLastBtn = bBtnDown;
 
   if (nPatternLen == 0) {//Find out how long this pattern is
     for (nPatternCtr = 0; nPatternCtr < PATTERN_MAXLEN; nPatternCtr += 1) {
@@ -274,7 +358,6 @@ void loop() {
     }
   }
 
-  tCurrTime = gTime.pfGetTicks();
   if (tCurrTime - tLastChange > TICKS_LEDCHANGE) {
     nPatternCtr = nPatternStartIdx;
 
