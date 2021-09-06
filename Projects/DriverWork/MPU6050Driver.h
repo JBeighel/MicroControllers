43d7d85c76e33f61b7d5550182e3b007f7375966
JBeighel <vodobaas@gmail.com>
2021-09-06 09:30:06 -0400

    code doceumentation
    Adding more doxygen comments

--- Projects/DriverWork/MPU6050Driver.h
+++ Projects/DriverWork/MPU6050Driver.h
@@ -128,18 +128,21 @@
 		MPU6050Reg_WhoAmI		= 0x75,	/**< Responds with devices base I2C address */
 	} eMPU6050Reg_t;
 	
+	/**	@brief		Bit mask and field values for the Config register
+		@ingroup	mpu6050
+	*/
 	typedef enum eMPU6050RegCfg_t {
-		MPU6050RegCfg_SyncMask		= 0x38,
-		MPU6050RegCfg_SyncDis		= 0x00,
-		MPU6050RegCfg_SyncTemp		= 0x08,
-		MPU6050RegCfg_SyncGyroX		= 0x10,
-		MPU6050RegCfg_SyncGyroY		= 0x18,
-		MPU6050RegCfg_SyncGyroZ		= 0x20,
-		MPU6050RegCfg_SyncAccelX	= 0x28,
-		MPU6050RegCfg_SyncAccelY	= 0x30,
-		MPU6050RegCfg_SyncAccelZ	= 0x38,
+		MPU6050RegCfg_SyncMask		= 0x38,	/**< Bits used for the frame sync settings */
+		MPU6050RegCfg_SyncDis		= 0x00,	/**< Disable synchronization input */
+		MPU6050RegCfg_SyncTemp		= 0x08,	/**< Sample the temperature on Frame Sync input */
+		MPU6050RegCfg_SyncGyroX		= 0x10,	/**< Sample the gyrometer X on Frame Sync input */
+		MPU6050RegCfg_SyncGyroY		= 0x18,	/**< Sample the gyrometer Y on Frame Sync input */
+		MPU6050RegCfg_SyncGyroZ		= 0x20,	/**< Sample the gyrometer Z on Frame Sync input */
+		MPU6050RegCfg_SyncAccelX	= 0x28,	/**< Sample the accelerometer X on Frame Sync input */
+		MPU6050RegCfg_SyncAccelY	= 0x30,	/**< Sample the accelerometer Y on Frame Sync input */
+		MPU6050RegCfg_SyncAccelZ	= 0x38,	/**< Sample the accelerometer Z on Frame Sync input */
 		
-		MPU6050RegCfg_LPFMask		= 0x07,
+		MPU6050RegCfg_LPFMask		= 0x07,	/**< Bits used for digital low pass filter settings */
 		MPU6050RegCfg_LPF260Hz		= 0x00,
 		MPU6050RegCfg_LPF180Hz		= 0x01,
 		MPU6050RegCfg_LPF90Hz		= 0x02,
@@ -149,46 +152,58 @@
 		MPU6050RegCfg_LPF5Hz		= 0x06,
 	} eMPU6050RegCfg_t;
 	
+	/**	@brief		Bit mask and field values for the Gyrometer Config register
+		@ingroup	mpu6050
+	*/
 	typedef enum eMPU6050RegGCfg_t {
 		MPU6050RegGCfg_XSelfTest	= 0x80,
 		MPU6050RegGCfg_YSelfTest	= 0x40,
 		MPU6050RegGCfg_ZSelfTest	= 0x20,
-		MPU6050RegGCfg_FullScaleMsk	= 0x18,
+		MPU6050RegGCfg_FullScaleMsk	= 0x18,	/**< Mask for bits setting the full scale reading */
 		MPU6050RegGCfg_FS250		= 0x00,
 		MPU6050RegGCfg_FS500		= 0x08,
 		MPU6050RegGCfg_FS1000		= 0x10,
 		MPU6050RegGCfg_FS2000		= 0x18,
 	} eMPU6050RegGCfg_t;
 		
+	/**	@brief		Bit mask and field values for the Accelerometer Config register
+		@ingroup	mpu6050
+	*/
 	typedef enum eMPU6050RegACfg_t {
 		MPU6050RegACfg_XSelfTest	= 0x80,
 		MPU6050RegACfg_YSelfTest	= 0x40,
 		MPU6050RegACfg_ZSelfTest	= 0x20,
-		MPU6050RegACfg_FullScaleMsk	= 0x18,
+		MPU6050RegACfg_FullScaleMsk	= 0x18,	/**< Mask for bits setting the full scale reading */
 		MPU6050RegACfg_FS2g			= 0x00,
 		MPU6050RegACfg_FS4g			= 0x08,
 		MPU6050RegACfg_FS8g			= 0x10,
 		MPU6050RegACfg_FS16g		= 0x18,
 	} eMPU6050RegACfg_t;
 	
+	/**	@brief		Bit mask and field values for the FIFO enable register
+		@ingroup	mpu6050
+	*/
 	typedef enum eMPU6050RegFIFOEn_t {
-		MPU6050RegFIFOen_None		= 0x00,
-		MPU6050RegFIFOEn_Temp		= 0x80,
-		MPU6050RegFIFOen_GyroX		= 0x40,
-		MPU6050RegFIFOen_GyroY		= 0x20,
-		MPU6050RegFIFOen_GyroZ		= 0x10,
-		MPU6050RegFIFOen_AccelX		= 0x08,
-		MPU6050RegFIFOen_Slave2		= 0x04,
-		MPU6050RegFIFOen_Slave1		= 0x02,
-		MPU6050RegFIFOen_Slave0		= 0x01,
+		MPU6050RegFIFOen_None		= 0x00,	/**< No values sent to FIFO buffer */
+		MPU6050RegFIFOEn_Temp		= 0x80,	/**< Send temperature readings to FIFO */
+		MPU6050RegFIFOen_GyroX		= 0x40,	/**< Send gyrometer X readings to FIFO */
+		MPU6050RegFIFOen_GyroY		= 0x20,	/**< Send gyrometer Y readings to FIFO */
+		MPU6050RegFIFOen_GyroZ		= 0x10,	/**< Send gyrometer Z readings to FIFO */
+		MPU6050RegFIFOen_AccelX		= 0x08,	/**< Send accelerometer X readings to FIFO */
+		MPU6050RegFIFOen_Slave2		= 0x04,	/**< Send I2C slave 2 readings to FIFO */
+		MPU6050RegFIFOen_Slave1		= 0x02,	/**< Send I2C slave 1 readings to FIFO */
+		MPU6050RegFIFOen_Slave0		= 0x01,	/**< Send I2C slave 0 readings to FIFO */
 	} eMPU6050RegFIFOEn_t;
 	
+	/**	@brief		Bit mask and field values for the I2C Master Config register
+		@ingroup	mpu6050
+	*/
 	typedef enum eMPU6050RegI2CMst_t {
 		MPU6050RegI2CMst_MultiMstr	= 0x80,
 		MPU6050RegI2CMst_WaitForExt	= 0x40,
-		MPU6050RegI2CMst_Slave3FIFO	= 0x20,
+		MPU6050RegI2CMst_Slave3FIFO	= 0x20,	/**< Send I2C slave 3 readings to FIFO */
 		MPU6050RegI2CMst_PNSR		= 0x10,
-		MPU6050RegI2CMst_MstClcMask	= 0x0F,
+		MPU6050RegI2CMst_MstClcMask	= 0x0F,	/**< Bits used for I2C master clock speed */
 		MPU6050RegI2CMst_Clk348kHz	= 0x00,
 		MPU6050RegI2CMst_Clk333kHz	= 0x01,
 		MPU6050RegI2CMst_Clk320kHz	= 0x02,
@@ -207,6 +222,9 @@
 		MPU6050RegI2CMst_Clk364kHz	= 0x0F,
 	} eMPU6050RegI2CMst_t;
 	
+	/**	@brief		Bit mask and field values for the Interrupt Config register
+		@ingroup	mpu6050
+	*/
 	typedef enum eMPU6050RegIntCfg_t {
 		MPU6050RegIntCfg_Level		= 0x80,
 		MPU6050RegIntCfg_Open		= 0x40,
@@ -218,30 +236,42 @@
 		MPU6050RegIntCfg_None		= 0x00,
 	} eMPU6050RegIntCfg_t;
 	
+	/**	@brief		Bit mask and field values for the Interrupt Enable register
+		@ingroup	mpu6050
+	*/
 	typedef enum eMPU6050RegIntEn_t {
-		MPU6050RegIntEn_None		= 0x00,
-		MPU6050RegIntEn_FIFOOFlow	= 0x10,
-		MPU6050RegIntEn_I2CMaster	= 0x40,
-		MPU6050RegIntEn_DataReady	= 0x01,
+		MPU6050RegIntEn_None		= 0x00,	/**< Do not trigger the interrupt pin */
+		MPU6050RegIntEn_FIFOOFlow	= 0x10,	/**< Send an interrupt when the FIFO overflows */
+		MPU6050RegIntEn_I2CMaster	= 0x40,	/**< Send an interrupt when the I2C master receives data */
+		MPU6050RegIntEn_DataReady	= 0x01,	/**< Send an interrupt when sample data is ready */
 	} eMPU6050RegIntEn_t;
 	
+	/**	@brief		Bit mask and field values for the Signal path reset register
+		@ingroup	mpu6050
+	*/
 	typedef enum eMPU6050RegSigRst_t {
-		MPU6050RegSigRst_All		= 0x07,
-		MPU6050RegSigRst_Gyro		= 0x04,
-		MPU6050RegSigRst_Accel		= 0x02,
-		MPU6050RegSigRst_Temp		= 0x01,
+		MPU6050RegSigRst_All		= 0x07,	/**< Reset all input signal paths */
+		MPU6050RegSigRst_Gyro		= 0x04,	/**< Reset gyrometer input signal paths */
+		MPU6050RegSigRst_Accel		= 0x02,	/**< Reset accelerometer input signal paths */
+		MPU6050RegSigRst_Temp		= 0x01,	/**< Reset temperature input signal paths */
 	} eMPU6050RegSigRst_t;
 	
+	/**	@brief		Bit mask and field values for the User control register
+		@ingroup	mpu6050
+	*/
 	typedef enum eMPU6050RegUsrCtrl_t {
-		MPU6050RegUsrCtrl_6050Def	= 0x00,
-		MPU6050RegUsrCtrl_FIFOEn	= 0x40,
-		MPU6050RegUsrCtrl_I2CMstEn	= 0x20,
+		MPU6050RegUsrCtrl_6050Def	= 0x00,	/**< Default settings for MPU-6050 */
+		MPU6050RegUsrCtrl_FIFOEn	= 0x40,	/**< Set to enable use of the FIFO */
+		MPU6050RegUsrCtrl_I2CMstEn	= 0x20,	/**< Set to enable teh I2C master */
 		MPU6050RegUsrCtrl_SPIEn		= 0x10,	/**< Enables the SPI interface and disables I2C */
-		MPU6050RegUsrCtrl_FIFORst	= 0x40,
-		MPU6050RegUsrCtrl_I2CMstRst	= 0x20,
+		MPU6050RegUsrCtrl_FIFORst	= 0x40,	/**< Set to reset all data in the FIFO */
+		MPU6050RegUsrCtrl_I2CMstRst	= 0x20,	/**< Set to reset the I2C master */
 		MPU6050RegUsrCtrl_SigCndRst	= 0x01,
 	} eMPU6050RegUsrCtrl_t;
 	
+	/**	@brief		Bit mask and field values for the Power management 1 register
+		@ingroup	mpu6050
+	*/
 	typedef enum eMPU6050RegPwr1_t {
 		MPU6050RegPwr1_Reset		= 0x80,	/**< Set to reset all internal registers to default */
 		MPU6050RegPwr1_Sleep		= 0x40,
@@ -257,6 +287,9 @@
 		MPU6050RegPwr1_ClkStop		= 0x07,
 	} eMPU6050RegPwr1_t;
 	
+	/**	@brief		Bit mask and field values for the Power management 2 register
+		@ingroup	mpu6050
+	*/
 	typedef enum eMPU6050RegPwr2_t {
 		MPU6050RegPwr2_LPWake1Hz	= 0x00,
 		MPU6050RegPwr2_LPWake5Hz	= 0x40,
@@ -272,10 +305,13 @@
 		MPU6050RegPwr2_StdByGyroZ	= 0x01,
 	} eMPU6050RegPwr2_t;
 	
+	/**	@brief		Driver object for the MPU-6050 peripheral
+		@ingroup	mpu6050
+	*/
 	typedef struct sMPU6050Obj_t {
-		sI2CIface_t *pI2C;
+		sI2CIface_t *pI2C;		/**< Pointer to I2C bus controller */
 		
-		eMPU6050Addr_t eAddr;
+		eMPU6050Addr_t eAddr;	/**< I2C Address for the peripheral */
 	} sMPU6050Obj_t;
 	
 /*****	Constants	*****/
@@ -288,10 +324,19 @@
 
 
 /*****	Functions	*****/
+	/**	@brief		Initialize driver object and send default configuration to peripheral
+		@ingroup	mpu6050
+	*/
 	eMPU6050Return_t MPU6050Initialize(sMPU6050Obj_t *pObj, sI2CIface_t *pI2C, eMPU6050Addr_t eI2CAddr);
 
+	/**	@brief		Read all gyrometer measurements from the peripheral
+		@ingroup	mpu6050
+	*/
 	eMPU6050Return_t MPU6050ReadGyro(sMPU6050Obj_t *pObj, int16_t *pnXMeas, int16_t *pnYMeas, int16_t *pnZMeas);
 	
+	/**	@brief		Read all accelerometer measurements from the peripheral
+		@ingroup	mpu6050
+	*/
 	eMPU6050Return_t MPU6050ReadAccel(sMPU6050Obj_t *pObj, int16_t *pnXMeas, int16_t *pnYMeas, int16_t *pnZMeas);
 
 #endif
